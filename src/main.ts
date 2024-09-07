9c37412a06f4875dfd561180e272e96a65e78b3a
samutrujillo <samueltrujillorodriguez@gmail.com>
2024-09-06 22:46:45 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://movieapp-u11o.onrender.com/', // Permitir el acceso desde Next.js
+    origin: ['https://movieapp-u11o.onrender.com/', 'http://localhost:3000'], // Permitir el acceso desde Next.js
+    credentials: true, 
   });
   await app.listen(process.env.PORT || 3080);
 }
